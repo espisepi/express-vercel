71605d24e178d65cfb9e33fe368c96e73d9b565b
espisepi <sepinaco@gmail.com>
2021-01-24 02:57:38 +0100

    added yt

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 const { Router } = require('express');
 const demo = require('./demo.route');
 
+// const fs = require('fs');
+const ytdl = require('ytdl-core');
+
 const r = Router();
 
 r.use('/demo', demo);
+
+r.get('/download', (req,res) => {
+  var URL = req.query.URL;
+  console.log(URL)
+  res.header('Content-Disposition', 'attachment; filename="video.mp4"');
+  ytdl(URL, {
+      format: 'mp4'
+      }).pipe(res);
+});
 
 r.get('/', (req, res) => {
   res.json({
